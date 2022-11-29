6c9e53a7da4cdba04f7b71372e5de2d6f7d6dfa5
nguyentamthanh <thanhh16012k@gmail.com>
2022-11-29 17:59:24 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup>
+useHead({
+  title: "Weather App",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/nuxt.png",
+    },
+  ],
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <div>trang chu</div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "defaults",
+});
+
+const y = 60;
+const x = 20;
+console.log("🚀 ~ file: index.vue ~ line 5 ~ x", x);
+</script>
